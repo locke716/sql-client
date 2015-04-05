d4c110cfa540aa759891be4bc472d5c6b7713043
PJ Rowe <locke716@gmail.com>
2015-04-04 23:47:50 -0400

    Initial commit

---
+++ src/main/java/com/prowe/sqlclient/Application.java
@@ -0,0 +1,14 @@
+package com.prowe.sqlclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setShowBanner(false);
+	}
+
+}
